d93b2759a2ee0a2b520456d0314361e8a43c11e8
Krunoslav Husak <kruno@husak.me>
2023-12-27 23:14:41 +0100

    Added route for handling SPA

--- backend/app/config/controllers.go
+++ backend/app/config/controllers.go
@@ -7,11 +7,15 @@ import (
 
 func Controllers() raptor.Controllers {
 	lc := &controllers.LotteriesController{}
+	sc := &controllers.SPAController{}
 
 	return raptor.RegisterControllers(
 		raptor.RegisterController("Lotteries", &lc.Controller,
 			raptor.Action("Get6of45", lc.Get6of45),
 			raptor.Action("Get7of39", lc.Get7of39),
+		),
+		raptor.RegisterController("SPA", &sc.Controller,
+			raptor.Action("Index", sc.Index),
 		),
 	)
 }
--- backend/app/config/routes.go
+++ backend/app/config/routes.go
@@ -8,5 +8,6 @@ func Routes() raptor.Routes {
 	return raptor.Routes{
 		raptor.Route("GET", "/api/v1/lotteries/6of45", "Lotteries", "Get6of45"),
 		raptor.Route("GET", "/api/v1/lotteries/7of39", "Lotteries", "Get7of39"),
+		raptor.Route("GET", "*", "SPA", "Index"),
 	}
 }
