55b3f7b1579ae679310821aaf7cac83f0caf25b3
Krunoslav Husak <kruno@husak.me>
2024-01-08 19:26:54 +0100

    Using scopes in routes config

--- backend/config/routes.go
+++ backend/config/routes.go
@@ -3,9 +3,13 @@ package config
 import "github.com/h00s/raptor"
 
 func Routes() raptor.Routes {
-	return raptor.Routes{
-		raptor.Route("GET", "/api/v1/lotteries/6of45", "LotteriesController", "Get6of45"),
-		raptor.Route("GET", "/api/v1/lotteries/7of39", "LotteriesController", "Get7of39"),
+	return raptor.CollectRoutes(
+		raptor.Scope("/api/v1",
+			raptor.Scope("/lotteries",
+				raptor.Route("GET", "/6of45", "LotteriesController", "Get6of45"),
+				raptor.Route("GET", "/7of39", "LotteriesController", "Get7of39"),
+			),
+		),
 		raptor.Route("GET", "*", "SPAController", "Index"),
-	}
+	)
 }
