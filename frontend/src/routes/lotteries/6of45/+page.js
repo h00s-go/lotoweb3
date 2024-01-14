801f9dfe9398cfaae798545436365f9840291ede
Krunoslav Husak <kruno@husak.me>
2024-01-14 17:13:15 +0100

    Updated urls for fetching numbers

--- frontend/src/routes/lotteries/6of45/+page.js
+++ frontend/src/routes/lotteries/6of45/+page.js
@@ -1,7 +1,7 @@
 import { PUBLIC_API_URL } from '$env/static/public'
 
 export async function load({ fetch, params }) {
-  const response = await fetch(PUBLIC_API_URL + '/lotteries/6of45');
+  const response = await fetch(PUBLIC_API_URL + '/lotteries/pick-one?numbers=6&max=45');
   const numbers = await response.json();
 
   return {
--- frontend/src/routes/lotteries/7of35/+page.js
+++ frontend/src/routes/lotteries/7of35/+page.js
@@ -1,7 +1,7 @@
 import { PUBLIC_API_URL } from '$env/static/public'
 
 export async function load({ fetch, params }) {
-  const response = await fetch(PUBLIC_API_URL + '/lotteries/7of35');
+  const response = await fetch(PUBLIC_API_URL + '/lotteries/pick-one?numbers=7&max=35');
   const numbers = await response.json();
 
   return {
